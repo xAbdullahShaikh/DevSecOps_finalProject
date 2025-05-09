4f0b3472037ec08f3f9b41be06493ae7d47736c6
C0D3NaM3AB <123714247+xAbdullahShaikh@users.noreply.github.com>
2025-05-10 03:00:26 +0500

    Update s3-positive.tf

--- test-cases/iac/s3-positive.tf
+++ test-cases/iac/s3-positive.tf
@@ -1,17 +1,1 @@
-resource "aws_s3_bucket" "secure_bucket" {
-  bucket = "my-secure-bucket"
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = aws_s3_bucket.secure_bucket.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-
-
 
