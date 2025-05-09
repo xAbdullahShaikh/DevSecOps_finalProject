93bd9821aba6ee68839b9f5491aae05143de982b
C0D3NaM3AB <123714247+xAbdullahShaikh@users.noreply.github.com>
2025-05-10 02:57:13 +0500

    Update s3-positive.tf

--- test-cases/iac/s3-positive.tf
+++ test-cases/iac/s3-positive.tf
@@ -14,22 +14,4 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
 
 
 
-resource "aws_security_group" "restricted_sg" {
-  name        = "restricted_sg"
-  description = "Allow limited inbound traffic"
-  vpc_id      = "vpc-abc123"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
