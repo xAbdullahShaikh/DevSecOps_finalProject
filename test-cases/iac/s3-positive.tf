9d84b35f7cc32e2f6df3f1fa8c3b5282c99a0aa7
C0D3NaM3AB <123714247+xAbdullahShaikh@users.noreply.github.com>
2025-05-10 11:24:32 +0500

    Update s3-positive.tf

--- test-cases/iac/s3-positive.tf
+++ test-cases/iac/s3-positive.tf
@@ -1,1 +1,19 @@
 
+resource "aws_s3_bucket" "secure_bucket" {
+  bucket = "secure-bucket-example"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_security_group" "restricted_sg" {
+  name        = "restricted"
+  description = "Restricted access"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+}
