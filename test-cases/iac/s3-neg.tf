c61b959a68d16baf6b806f9111f05bb70d5ff82e
C0D3NaM3AB <123714247+xAbdullahShaikh@users.noreply.github.com>
2025-05-10 11:24:10 +0500

    Update s3-neg.tf

--- test-cases/iac/s3-neg.tf
+++ test-cases/iac/s3-neg.tf
@@ -1,6 +1,15 @@
-resource "aws_s3_bucket" "insecure_bucket" {
-  bucket = "my-insecure-bucket"
+resource "aws_s3_bucket" "public_bucket" {
+  bucket = "public-bucket-example"
+  acl    = "public-read"  # ❌ Checkov will flag this
 }
 
-
-
+resource "aws_security_group" "open_sg" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # ❌ Too permissive
+  }
+}
